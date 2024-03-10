50a3a0f703bcf785162a229901be838c0c63edbf
GentleHorse <anch.abc.t@gmail.com>
2024-03-10 17:18:24 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Working with Physics in R3F
---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { Canvas } from "@react-three/fiber";
+import Experience from "./components/Experience.jsx";
+
+export default function App() {
+  return (
+    <Canvas
+      shadows
+      camera={{
+        fov: 45,
+        near: 0.1,
+        far: 200,
+        position: [2.5, 4, 6],
+      }}
+    >
+      <Experience />
+    </Canvas>
+  );
+}
